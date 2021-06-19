290590b4a31a574b3c1b275dcea6697acac61537
PetitLuc38 <78622151+PetitLuc38@users.noreply.github.com>
2021-06-19 13:52:33 +0200

    Update tg-python.rb

--- tg-python.rb
+++ tg-python.rb
@@ -1,4 +1,4 @@
-class TGPython < Formula
+class TgPython < Formula
   desc "Tool for timing mechanical watches"
   homepage "https://tg.ciovil.li"
   url "https://github.com/xyzzy42/tg.git", :tag => "v0.6.0-tpiepho"
